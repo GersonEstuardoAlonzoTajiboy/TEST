90e95adf363219e07a295b2db7fc6c3c28ab1b10
GEAT <111700508+GersonEstuardoAlonzoTajiboy@users.noreply.github.com>
2024-11-15 20:08:52 -0600

    UPDATE
    UPDATE

--- testSynJSX-main/frontend/src/components/layout/sidebar.tsx
+++ testSynJSX-main/frontend/src/components/layout/sidebar.tsx
@@ -37,38 +37,38 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
   };
 
   return (
-    <div className={`bg-orange-500 h-full ${isOpen ? 'w-[250px]' : 'w-[88px]'} transition-all duration-300 flex flex-col p-3 shadow-lg`}>
-      <button onClick={handleToggleSidebar} className="mb-4 p-2 focus:outline-none bg-orange-500 hover:bg-orange-500 rounded-md">
+    <div className={`bg-purple-500 h-full ${isOpen ? 'w-[250px]' : 'w-[88px]'} transition-all duration-300 flex flex-col p-3 shadow-lg`}>
+      <button onClick={handleToggleSidebar} className="mb-4 p-2 focus:outline-none bg-purple-500 hover:bg-purple-500 rounded-md">
         {isOpen ? <img src="src/assets/icon/close-menu.svg" alt="Close Menu" className="h-10 w-auto" /> : <img src="src/assets/icon/open-menu.svg" alt="Open Menu" className="h-10 w-auto" />}
       </button>
 
       <div className="flex flex-col space-y-4">
-        <button onClick={() => handleNavigation('/dashboard')} className="flex items-center bg-orange-500 p-2 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
+        <button onClick={() => handleNavigation('/dashboard')} className="flex items-center bg-purple-500 p-2 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
           <img src="src/assets/icon/projects.svg" alt="Proyectos" className="w-12 h-12" />
           {isOpen && <span className="ml-2 p-2 text-lg font-medium">Proyectos</span>}
         </button>
 
-        <button onClick={() => handleNavigation('/calendar')} className="flex items-center bg-orange-500 p-2 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
+        <button onClick={() => handleNavigation('/calendar')} className="flex items-center bg-purple-500 p-2 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
           <img src="src/assets/icon/calendar.svg" alt="Calendar" className="w-12 h-12" />
           {isOpen && <span className="ml-2 p-2 text-lg font-medium">Cronograma</span>}
         </button>
 
-        <button onClick={() => handleNavigation('/board')} className="flex items-center bg-orange-500 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
+        <button onClick={() => handleNavigation('/board')} className="flex items-center bg-purple-500 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
           <img src="src/assets/icon/boad.svg" alt="board" className="w-12 h-12" />
           {isOpen && <span className="ml-2 p-2 text-lg font-medium">Tablero</span>}
         </button>
 
-        <button onClick={() => handleNavigation('/planning')} className="flex items-center bg-orange-500 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
+        <button onClick={() => handleNavigation('/planning')} className="flex items-center bg-purple-500 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
           <img src="src/assets/icon/plan.svg" alt="palan" className="w-12 h-12" />
           {isOpen && <span className="ml-2 p-2 text-lg font-medium">Planificación de Pruebas</span>}
         </button>
 
-        <button onClick={() => handleNavigation('/exe-test')} className="flex items-center bg-orange-500 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
+        <button onClick={() => handleNavigation('/exe-test')} className="flex items-center bg-purple-500 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
           <img src="src/assets/icon/execute.svg" alt="execute" className="w-12 h-12" />
           {isOpen && <span className="ml-2 p-2 text-lg font-medium">Ejecución de Pruebas</span>}
         </button>
 
-        <button onClick={() => handleNavigation('/result')} className="flex items-center bg-orange-500 p-2 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
+        <button onClick={() => handleNavigation('/result')} className="flex items-center bg-purple-500 p-2 rounded-lg shadow-md hover:bg-orange-500 w-full text-left">
           <img src="src/assets/icon/infop.svg" alt="result" className="w-12 h-12" />
           {isOpen && <span className="ml-2 p-2 text-lg font-medium">Resultados de Proyectos</span>}
         </button>
@@ -76,21 +76,21 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
       </div>
 
       <div className="space-y-6 mt-auto">
-        <button className="flex items-center bg-orange-500 p-2 rounded-lg shadow-md hover:bg-orange-500 w-full text-left" onClick={() => setShowConfirmLogout(true)}>
+        <button className="flex items-center bg-purple-500 p-2 rounded-lg shadow-md hover:bg-purple-500 w-full text-left" onClick={() => setShowConfirmLogout(true)}>
           <img src="src/assets/icon/logout.svg" alt="Salir" className="w-12 h-12" />
           {isOpen && <span className="ml-2 p-2 text-lg font-medium">Cerrar Sesión</span>}
         </button>
       </div>
 
       {showConfirmLogout && (
-        <div className="fixed inset-0 bg-orange-500 bg-opacity-75 flex justify-center items-center z-50">
+        <div className="fixed inset-0 bg-purple-500 bg-opacity-75 flex justify-center items-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg z-60">
             <h2 className="text-xl mb-4">¿Seguro que quieres cerrar la sesión?</h2>
             <div className="flex justify-between">
               <button className="bg-[#042354] hover:bg-[#28559c] text-white px-4 py-2 rounded " onClick={handleLogout}>
                 Cerrar Sesión
               </button>
-              <button className="bg-orange-500 text-gray-700 px-4 py-2 rounded hover:bg-orange-500" onClick={() => setShowConfirmLogout(false)}>
+              <button className="bg-purple-500 text-gray-700 px-4 py-2 rounded hover:bg-purple-500" onClick={() => setShowConfirmLogout(false)}>
                 Cancelar
               </button>
             </div>
